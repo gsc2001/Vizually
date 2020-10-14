02005b84af0d58b13e81d6c1a6a873a308635af2
Kannav Mehta <kmkannavkmehta@gmail.com>
2020-10-15 00:16:04 +0530

    trying qml:

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
+import json
+import os
 import sys
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtUiTools import QUiLoader
+import urllib.request
+
+from PySide2.QtCore import QStringListModel, QUrl
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQuick import QQuickView
 
 if __name__ == '__main__':
-	loader = QUiLoader()
-	app = QtWidgets.QApplication(sys.argv)
-	window = loader.load("mainwindow.ui", None)
-	window.show()
-	app.exec_()
+
+    # get our data
+    url = "http://country.io/names.json"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode('utf-8'))
+
+    # Format and sort the data
+    data_list = list(data.values())
+    data_list.sort()
+
+    # Set up the application window
+    app = QGuiApplication(sys.argv)
+    view = QQuickView()
+    view.setResizeMode(QQuickView.SizeRootObjectToView)
+
+    # Expose the list to the Qml code
+    my_model = QStringListModel()
+    my_model.setStringList(data_list)
+    view.rootContext().setContextProperty("myModel", my_model)
+
+    # Load the QML file
+    qml_file = os.path.join(os.path.dirname(__file__), "view.qml")
+    view.setSource(QUrl.fromLocalFile(os.path.abspath(qml_file)))
+
+    # Show the window
+    if view.status() == QQuickView.Error:
+        sys.exit(-1)
+    view.show()
+
+    # execute and cleanup
+    app.exec_()
+    del view
