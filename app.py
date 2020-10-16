c8b77be72e087503fdcf431869975646e3f229b8
Kannav Mehta <kmkannavkmehta@gmail.com>
2020-10-16 20:38:12 +0530

    some changes

--- app.py
+++ app.py
@@ -9,24 +9,10 @@ from PySide2.QtQuick import QQuickView
 
 if __name__ == '__main__':
 
-    # get our data
-    url = "http://country.io/names.json"
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read().decode('utf-8'))
-
-    # Format and sort the data
-    data_list = list(data.values())
-    data_list.sort()
-
     # Set up the application window
     app = QGuiApplication(sys.argv)
     view = QQuickView()
     view.setResizeMode(QQuickView.SizeRootObjectToView)
-
-    # Expose the list to the Qml code
-    my_model = QStringListModel()
-    my_model.setStringList(data_list)
-    view.rootContext().setContextProperty("myModel", my_model)
 
     # Load the QML file
     qml_file = os.path.join(os.path.dirname(__file__), "view.qml")
