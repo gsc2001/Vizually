c11f6a2a78f48641a01b639a6b3d6dc70865db8a
Triansh <trianshsharma@gmail.com>
2020-10-25 15:33:39 +0530

    Added Flipping, Rotating, Perspective transformations

---
+++ vizually/core/modules/flip.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+from models.image import Image
+
+
+def flipHandler(image: Image, params : dict)-> Image:
+    """ Flipping Image Handler
+
+    Args:
+        image (np.array): image to change
+        params (dict): params has { flipCode : int}
+
+        THE FLIPCODE MUST BE AMONG 
+         1 : to flip horizontally
+         0 : to flip vertically
+        -1 : to flip both horizontally and vertically
+    
+    Returns:
+        np.array: Flipped image
+
+    """
+
+    new_img = flipImage(image.img, params['flipCode'])
+    new_image = Image(path=image.path, img=new_img)
+    return new_image
+
+
+def flipImage(image:Image, flipCode: int)-> Image:
+    """Flipping image
+    Args:
+        image (np.array): image to change
+        flipCode (int) : the type of flip
+
+    Returns:
+        np.array: Flipped image
+    """
+    return cv2.flip(image, flipCode)
