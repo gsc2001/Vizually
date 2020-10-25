d6d5b558dd4b5a3489dfc64f45f7d932f3b7cdab
shreygupta2809 <shrey2809@gmail.com>
2020-10-25 10:31:26 +0530

    Updated sharpen and histogramManipulation

--- vizually/core/modules/histogramManipulation.py
+++ vizually/core/modules/histogramManipulation.py
@@ -9,29 +9,29 @@ def histogramManipulationHandler(image: Image, params: dict) -> Image:
 
     Args:
         image (np.array): image to change
-        params (dict): params has { clip_limit: float }
+        params (dict): params has { contrast_limit: float ((0, 3], step size of 0.1) }
 
     Returns:
         np.array: manipulated image
     """
     
-    new_img = contrastManipulation(image.img, params['clip_limit'])
+    new_img = contrastManipulation(image.img, params['contrast_limit'])
     new_image = Image(path=image.path, img=new_img)
     return new_image
 
 
-def contrastManipulation(image: np.array, clip_limit: float) -> np.array:
+def contrastManipulation(image: np.array, contrast_limit: float) -> np.array:
     """Manipulate Histogram of the image
 
     Args:
         image (np.array): image to change
-        clip_limit (float): Clip Limit to choose
+        contrast_limit (float): Contrast Limit (clip limit) to choose
 
     Returns:
         np.array: Manipulated (Changed Contrast) image
     """
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit = clip_limit)
+    clahe = cv2.createCLAHE(clipLimit = contrast_limit)
     final_img = clahe.apply(gray)
     return final_img
--- vizually/core/modules/sharpening.py
+++ vizually/core/modules/sharpening.py
@@ -9,24 +9,23 @@ def sharpenHandler(image: Image, params: dict) -> Image:
 
     Args:
         image (np.array): image to change
-        params (dict): params has { kernel_size: int (odd), sigma: float (> 0), strength: float}
+        params (dict): params has { kernel_size: int (odd, [1, 3, 5, 7, 9, 11]), strength: float ([0 - 10], step size of 0.5)}
 
     Returns:
         np.array: Sharpened image
     """
     
-    new_img = sharpenImage(image.img, params['kernel_size'], params['sigma'], params['strength'])
+    new_img = sharpenImage(image.img, params['kernel_size'], params['strength'])
     new_image = Image(path=image.path, img=new_img)
     return new_image
 
 
-def sharpenImage(image: np.array, kernel_size: int, sigma: float, strength: float) -> np.array:
+def sharpenImage(image: np.array, kernel_size: int, strength: float) -> np.array:
     """Sharpening of the image
 
     Args:
         image (np.array): image to change
         kernel_size (int): Kernel Size to choose for filter
-        sigma (float): Sigma to choose for filter
         strength (float): Strength to choose
 
     Returns:
@@ -35,13 +34,14 @@ def sharpenImage(image: np.array, kernel_size: int, sigma: float, strength: floa
     final_img = np.zeros_like(image)
     num_channels = image.shape[2]
     for i in range(num_channels):
-        final_img[:,:,i] = unsharp(image[:,:,i], kernel_size, sigma, strength)
+        final_img[:,:,i] = unsharp(image[:,:,i], kernel_size, strength)
     
     return final_img
 
-def unsharp(image, kernel_size, sigma, strength):
+def unsharp(image: np.array, kernel_size: int, strength: float) -> np.array:
 
-    gauss = cv2.GaussianBlur(image, (kernel_size, kernel_size), sigma)
-    sharpened_img = cv2.addWeighted(image, 1 + strength, gauss, -strength, 0)
+#     gauss = cv2.GaussianBlur(image, (kernel_size, kernel_size), sigma)
+    mf = cv2.medianBlur(image, kernel_size)
+    sharpened_img = cv2.addWeighted(image, 1 + strength, mf, -strength, 0)
     
     return sharpened_img
