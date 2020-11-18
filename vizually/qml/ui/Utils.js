825e95d9eee6e10346ff32197abb2ab9007cad4b
abakfja <kannav.krishan.mehta@gmail.com>
2020-11-18 13:31:00 +0530

    resolved merge conflicts. Ridge detect not working

---
+++ vizually/qml/ui/Utils.js
@@ -0,0 +1,13 @@
+
+function getHeight(par) {
+	var vis_cnt = 0
+	if (par.visible == 0) {
+		return 30
+	}
+	for(var i=1; i<par.children.length; i++){
+		if(par.children[i].visible == 1){
+			vis_cnt++
+		}
+	}
+	return 40 * vis_cnt + 90
+}
