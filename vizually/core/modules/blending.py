b0df4eb22e07760d1b4c6a0609bda0716664cfd9
Triansh Sharma <56212901+Triansh@users.noreply.github.com>
2020-11-20 00:01:50 +0530

    [Fix] Blending (#28)

--- vizually/core/modules/blending.py
+++ vizually/core/modules/blending.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 
 
-def BlendingHandler(image1: np.array, image2: np.array, params: dict) -> np.array:
+def BlendingHandler(primary_image: np.array, secondary_image: np.array, params: dict) -> np.array:
     """ Blending Image Handler
 
     Args:
-        image1 and image2 (np.array): images to blend
+        primary_image and secondary_image (np.array): images to blend
         params (dict): params has { alpha , contrast}
 
         alpha has range [0, 1] with STEP = 0.01
@@ -28,21 +28,21 @@ def BlendingHandler(image1: np.array, image2: np.array, params: dict) -> np.arra
     elif params['contrast'] < -50:
         params['contrast'] = -50
 
-    new_img = blendImages(image1, image2, float(
+    new_img = blendImages(primary_image, secondary_image, float(
         params['alpha']), float(params['contrast']))
     return new_img
 
 
-def blendImages(image1: np.array, image2: np.array, alpha: float, contrast: float) -> np.array:
+def blendImages(primary_image: np.array, secondary_image: np.array, alpha: float, contrast: float) -> np.array:
     """Blend 2 images
 
     Args:
-        image1 and image2 (np.array): images to blend
+        primary_image and secondary_image (np.array): images to blend
         alpha (float) : this is percentage of first image taken during blending
         contrast (float) : add this to increase/decrease pixel value for each cell in image (darken or brighten image)
 
     Returns:
         np.array: Blended image
     """
-
-    return cv2.addWeighted(image1, alpha, image2, 1 - alpha, contrast)
+    secondary_image = cv2.resize(secondary_image, (primary_image.shape[1], primary_image.shape[0]))
+    return cv2.addWeighted(primary_image, alpha, secondary_image, 1 - alpha, contrast)
