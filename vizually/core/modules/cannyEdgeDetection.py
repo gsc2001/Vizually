f52efa4b269694f52b764fed8d275f5baf5e2e56
Triansh Sharma <56212901+Triansh@users.noreply.github.com>
2020-11-22 13:58:21 +0530

    [Update] Canny Edge Detection and Splash (#30)

--- vizually/core/modules/cannyEdgeDetection.py
+++ vizually/core/modules/cannyEdgeDetection.py
@@ -9,38 +9,38 @@ def cannyEDHandler(image: np.array, params: dict) -> np.array:
 
     Args:
         image (np.array): image to change
-        params (dict): params has { strength}
+        params (dict): params has { min_threshold, max_threshold}
 
-        strength is used to calculate low and high thresholds
-        strength is in range [0, 127]
+        thresholds is in range [0, 255]
         step value = 1
 
     Returns:
         np.array:  image  after canny edge detection
     """
 
-
     if 'strength' not in params:
         return image
 
-    if params['strength'] > 127 :
-        params['strength'] = 127
-    elif  params['strength'] < 0 :
-        params['strength'] = 0
+    params['min_threshold'] = 255 if params['min_threshold'] > 255 else params['min_threshold']
+    params['min_threshold'] = 0 if params['min_threshold'] < 0 else params['min_threshold']
 
-    return cannyED(image, float(params['strength']))
+    params['max_threshold'] = 255 if params['max_threshold'] > 255 else params['max_threshold']
+    params['max_threshold'] = 0 if params['max_threshold'] < 0 else params['max_threshold']
+
+    return cannyED(image, round(min(params['max_threshold'], params['min_threshold'])),
+                   round(max(params['max_threshold'], params['min_threshold'])))
 
 
-def cannyED(image: np.array, strength: float) -> np.array:
+def cannyED(image: np.array, min_threshold: int, max_threshold: int) -> np.array:
     """Canny Edge detection technique
 
     Args:
         image (np.array): image to change
-        strength (float): For getting threshholds
+        threshold (float): For getting threshholds
 
     Returns:
         np.array: image after canny edge detection
     """
-    blurred_img = cv2.blur(image, (5, 5))
-    final_img = cv2.Canny(blurred_img, strength * .5, strength * 2)
+    blurred_img = cv2.blur(image, (7, 7))
+    final_img = cv2.Canny(blurred_img, min_threshold, max_threshold)
     return cv2.cvtColor(final_img, cv2.COLOR_GRAY2BGR)
