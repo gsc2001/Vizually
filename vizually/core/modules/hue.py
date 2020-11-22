abbfd5fd0879a9be7c28b6a2cfd4d51215adfba7
Chief-Blackhood <56213821+Chief-Blackhood@users.noreply.github.com>
2020-11-23 00:19:35 +0530

    [Add] Contour feature and hue

---
+++ vizually/core/modules/hue.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+
+def hueHandler(image: np.array, params: dict) -> np.array:
+    """hue filter handler
+
+    Args:
+        image(np.array): image to change
+        params (dict): params has { hue_value } range: (-179, 179) step size of 1
+
+    Returns:
+        np.array: Image having a hue filter
+    """
+
+    if 'hue_value' not in params:
+        return image
+
+    if params['hue_value'] >= 179:
+        params['hue_value'] = 179
+    elif params['hue_value'] < 0:
+        params['hue_value'] = 0
+
+    new_img = hueFilter(
+        image, float(params['hue_value']))
+    return new_img
+
+
+def hueFilter(image: np.array, hue_value: float) -> np.array:
+    """Apply hue filter on the image
+
+    Args:
+        image (np.array): image to change
+        hue_value (float): How much hue to increase
+
+    Returns:
+        np.array: hue filter applied image
+    """
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hsv[..., 0] = hsv[..., 0]+hue_value
+    hsv[..., 0][hsv[..., 0] > 179] = 179
+    hsv[..., 0][hsv[..., 0] < 0] = 0
+
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
