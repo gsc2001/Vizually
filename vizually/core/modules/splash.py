240663c524f90539ed7b25fec2850d9981584ab2
tichnas <sanchit.arora.2002@gmail.com>
2020-11-23 16:25:09 +0530

    Minor Fixes in Core

--- vizually/core/modules/splash.py
+++ vizually/core/modules/splash.py
@@ -10,7 +10,7 @@ def splashHandler(image: np.array, params: dict) -> np.array:
     Args:
         image (np.array): image to change
         params (dict): params has { min[i], max[i]} (multiple)
-        min_hue and max_hue both have range [0,255] with STEP = 1
+        min_hue and max_hue both have range [0,179] with STEP = 1
 
     Returns:
         np.array:  image  with extracted colors
@@ -18,13 +18,13 @@ def splashHandler(image: np.array, params: dict) -> np.array:
 
     strm = ("min", "max")
     min_max_list = []
-    for i in range(1, 10):
+    for i in range(10):
         m = (strm[0] + str(i), strm[1] + str(i))
         if m[0] not in params or m[1] not in params:
             break
         else:
-            params[m[0]] = 255 if params[m[0]] > 255 else params[m[0]]
-            params[m[1]] = 255 if params[m[1]] > 255 else params[m[1]]
+            params[m[0]] = 179 if params[m[0]] > 179 else params[m[0]]
+            params[m[1]] = 179 if params[m[1]] > 179 else params[m[1]]
             params[m[0]] = 0 if params[m[0]] < 0 else params[m[0]]
             params[m[1]] = 0 if params[m[1]] < 0 else params[m[1]]
 
@@ -50,7 +50,7 @@ def getColors(image: np.array, min_max_list: list) -> np.array:
     final_img = cv2.bitwise_and(image, image, mask=mask0)
 
     for minH, maxH in min_max_list:
-        mask = cv2.inRange(hsv, (minH, 0, 0), (maxH, 255, 255))
+        mask = cv2.inRange(hsv, (minH, 0, 0), (maxH, 179, 179))
         cur_img = cv2.bitwise_and(image, image, mask=mask)
         mask0 = mask0 | mask
         final_img = final_img | cur_img
